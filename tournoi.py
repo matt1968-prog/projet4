5ace8ef2aafdce955ee6c82ffa598b72235f7cee
Matt 1968 <pascal.amann.perso@gmail.com>
2022-11-27 22:37:05 +0100

    Ajout de quelques fichiers dans le MVC

--- tournoi.py
+++ tournoi.py
@@ -1,21 +1,17 @@
 from models.player import Joueur
 from views.tournoi import Tournoi
-from views.match import Match
-from views.round import Round
+#from views.match import Match
+from views.round import Tour
+from views.menu import Affichage_Menu
 import re
-import tinydb
+from tinydb import TinyDB
 
 def main():
 
-    """ MENU Vue ? Traitement du choix du menu dans Controlers et affichage du menu dans View ?
+    #MENU Vue ? Traitement du choix du menu dans Controlers et affichage du menu dans View ?
 
-    print("1. Nouveau tournoi et saisie des joueurs")
-    print("2. Affichage des matchs")
-    print("3. Saisie des résultats")
-    print("4. Match du tour suivant")
-    print("5. Afficher classement")
-    print("6. Quitter")"""
-
+    menu=Affichage_Menu
+    #print(menu)
 
     """NOUVEAU TOURNOI ET SAISIE DES JOUEURS (6 joueurs pour test)
 
@@ -23,7 +19,7 @@ def main():
     
     joueurs=[]
 
-    for i in range (0,6):
+    for i in range (1,7):
 
         print(f"Entrer le joueur {i}\n")
         
@@ -31,11 +27,11 @@ def main():
         
         prenom=input(f"Prénom du joueur {i}: \n")
         
-        date_naissance=input ("Date de naissance (format JJ/MM/AAA :)") #vérif dans la VUE
+        date_naissance=input ("Date de naissance :)") #vérif dans la VUE
         #date=re.search("^([1-9] |1[0-9]| 2[0-9]|3[0-1])(./-)([1-9] |1[0-2])(./-|)19[0-9][0-9]$",date_naissance)
         #print(date)
 
-        sexe=input("Sexe du joueur (H/F) :")
+        sexe=input("Sexe du joueur (H/F), par défaut H :")
 
         while True:
             try:
@@ -45,12 +41,11 @@ def main():
             except ValueError:
                 print("Le classement doit être un entier, positif et au moins égal à 1600")
         
-        joueur=Joueur(nom, prenom, date_naissance, sexe, rating, score=0)
+        joueur=Joueur(nom, prenom, date_naissance, rating, sexe, score=0)
         joueurs.append(joueur)
          
     for i in joueurs:
-        #print(f'{i.nom} {i.prenom} {i.date_naissance} {i.sexe}, {i.rating}')
-        print(f'{i.nom} {i.rating}')
+        print(f'{i.nom} {i.rating} {i.score}')
     
     """Descending sorting players by rank Contrôleur ou modèle ou View ?
 
@@ -68,10 +63,10 @@ def main():
 
     print(joueurs)
 
-    """#Création d'un tournoi
+    """#Crating a tournament
 
-   Creating 1 round (premier match), list of matches and add match to a list of matches
-    Add to the index of each of the 3 top players to determine opponent for 1st round"""
+   Creating 1st round (premier match), list of matches and add every match to a list of matches
+    Add 3 to the index of each of the 3 top players to determine opponent for 1st round"""
 
     matchs=[]
     tour=[]
@@ -84,7 +79,7 @@ def main():
         print(match)
         #print(matchs)
 
-    premier_tour=Round(1, matchs)
+    premier_tour=Tour(1, matchs)
     print(premier_tour)
 
 
@@ -99,24 +94,33 @@ def main():
     nouveau_tournoi=Tournoi(nom_tournoi,lieu_tournoi,date_tournoi,joueurs,type_tournoi,description)
     nouveau_tournoi.afficher_tournoi()
 
-    """Résultats des matchs et modification des scrores et classements   -> Controler"""
-    """print("Pour les résultats, 1=gain premier joueur, 2=gain du second joueur, 0=match nul")
-    saisie des résultats dans la vue
+    #Résultats des matchs et modification des scrores et classements#-> Controler
+    print("Pour les résultats, 1=gain premier joueur, 2=gain du second joueur, 0=match nul")
+    #saisie des résultats dans la vue
     for i in range(0,3):
-        resultat=int(input(f"Résultat du match entre {joueurs[i].nom} et {joueur[i+3].nom}"))
+        print(joueurs[i].nom +" vs "+ joueurs[i+3].nom) 
+        resultat=int(input("Résultat : "))
         if resultat==1:
             joueurs[i].score+=1
-        elif result==2:
-            joueur[i+3]+=1
+        elif resultat==2:
+            joueurs[i+3].score+=1
         elif resultat==0:
             joueurs[i].score+=0.5
             joueurs[i+3].score+=0.5
         else:
-            print("Donnée non valable")    
-        print(joueurs[i].score +" " +joueurs[i+3].score) -> vue pour l'affichage, controler pour modif des scores
+            print("Donnée non valide")    
+        print(str(joueurs[i].score) +" " +str(joueurs[i+3].score))# -> vue pour l'affichage, controler pour modif des scores
         
-        Appeler ensuite fonction/méthode de classement au terme des résultats du tour
-           """        
-
+    for j in range(0,5):# utiliser sort -> dans Views
+        for i in range(0,5):
+            joueur=joueurs[i].score
+            if joueurs[i+1].score>joueur:
+                joueurs[i], joueurs[i+1]=joueurs[i+1], joueurs[i]
+                
+    print("\nClassement après le 1er match : \n")        
+    for i in joueurs:
+        print(i.nom, i.prenom, i.score)    
+        #Appeler ensuite fonction/méthode de classement au terme des résultats du tour
+            
 if __name__ == "__main__":
     main()
