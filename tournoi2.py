2238a1417ced7144e6145535493ee5306135deb1
Matt 1968 <pascal.amann.perso@gmail.com>
2022-12-01 20:37:05 +0100

    MAJ des rencontres jouées après le 1er tour

--- tournoi2.py
+++ tournoi2.py
@@ -18,34 +18,6 @@ def main():
     """NOUVEAU TOURNOI ET SAISIE DES JOUEURS (6 joueurs pour test)
 
     Création d'un nouveau tournoi et de la liste des joueurs, classés par ELO, donc pour le 1er tour"""
-    
-    
-    #initialisation du tableau des matchs disputés. O si match non joué, 1 si match joué (ou i=j)
-    matchs_joues=[]
-    rows, cols = (6, 6)
-    for i in range (cols):
-        col=[]
-        for j in range(rows):
-            if i==j:
-                col.append(1)
-            else:
-                col.append(0)
-        matchs_joues.append(col)
-    print(matchs_joues)
-          
-            
-    #print (matchs_joues[0][0])
-    #print (matchs_joues[1][1])
-    
-    j=0
-    for i in range(0,6):
-        for j in range(6):
-            print(matchs_joues[i][j])
-            #print(j)
-            #if i!=j:
-                #matchs_joues.append(0)
-                #matchs_joues[i][j]=0
-            #j+=1
             
     joueurs=[]
     db = TinyDB('db.json')
@@ -61,7 +33,7 @@ def main():
         
         f_name=input(f"Prénom du joueur {i} : \n")
         
-        dob=input ("Date de naissance :)") #vérif dans la VUE
+        dob=input ("Date de naissance :") #vérif dans la VUE
         #date=re.search("^([1-9] |1[0-9]| 2[0-9]|3[0-1])(./-)([1-9] |1[0-2])(./-|)19[0-9][0-9]$",date_naissance)
         #print(date)
 
@@ -97,39 +69,65 @@ def main():
             if joueurs[i+1].rating>joueur:
                 joueurs[i], joueurs[i+1]=joueurs[i+1], joueurs[i]"""
     
-    #classement par ELo (descendant)
-    #classement_par_elo=classement_ELO(joueurs)
-    #classement_alpha=classement_alphabetique(joueurs)
-    joueurs.sort(key=lambda j: j.rating, reverse=True)
-    print("\nClassement par ELO : \n")        
-    for i in joueurs:
-        print(i.name, i.f_name, i.rating)
-    print(joueurs)
-
     #classement alphabétique
     
     joueurs.sort(key=lambda j: j.name, reverse=False)
     print("\nClassement par ordre alphabétique : \n")        
     for i in joueurs:
         print(i.name, i.f_name, i.rating)
-    print(joueurs)
+    #print(joueurs)
     
+    #classement par ELo (descendant)
+    
+    joueurs.sort(key=lambda j: j.rating, reverse=True)
+    print("\nClassement par ELO : \n")        
+    for i in joueurs:
+        print(i.name, i.f_name, i.rating)
+    #print(joueurs)
+    
+    #initialisation du tableau des matchs disputés. O si match non joué entre deux joueurs, 1 si match joué (ou i=j)
+    matchs_joues=[]
+    rows, cols = (6, 6)
+    for i in range (cols):
+        col=[]
+        for j in range(rows):
+            if i==j:
+                col.append(1)
+            else:
+                col.append(0)
+        matchs_joues.append(col)
+    print(matchs_joues) 
+    
+    j=0
+    """for i in range(0,6):
+        for j in range(6):
+            print(matchs_joues[i][j])
+            #print(j)
+            #if i!=j:
+                #matchs_joues.append(0)
+                #matchs_joues[i][j]=0
+            #j+=1"""
     
     """#Crating 1st round of a tournament
     Add 3 to the index of each of the 3 top players to determine opponent for 1st round"""
 
     matchs=[]
     tour=[]
-    nbre_jouers=6
+    nbre_joueurs=6
     for i in range(0,3):
         print(f"Match  {i} : ")
         print(joueurs[i].name +" vs "+ joueurs[i+3].name)
         match=([joueurs[i].name, joueurs[i].score], [joueurs[i+3].name, joueurs[i+3], joueurs[i+3].score])
         matchs.append(match)
         tour.append(match)
-        print(match)
+        matchs_joues[i][i+3]=1
+        for i in matchs:
+            print(i)
         #print(matchs)
 
+    print("Après le 1er tour, tableau des rencontres :")
+    print(matchs_joues)
+    
     premier_tour=Tour(1, matchs)
     print(premier_tour)
 
@@ -159,15 +157,17 @@ def main():
         elif resultat==0:
             joueurs[i].score+=0.5
             joueurs[i+3].score+=0.5
+            matchs_joues[i][z+3]+=0.5
+            matchs_joues[i][z+3]+=0.5
         else:
             print("Donnée non valide")    
         print(str(joueurs[i].score) +" " +str(joueurs[i+3].score))# -> vue pour l'affichage, controler pour modif des scores
         matchs_joues[i][z+3]=1
         
     # Affichage du tableau des matchs joueués ou non (0=non joué)
-    for i in range(6):
+    """for i in range(6):
         for j in range(6):
-            print(matchs_joues[i,j])
+            print(matchs_joues[i,j])"""
     
     #actualisation des scores des joueurs
     for j in range(0,5):# utiliser sort -> dans Views
@@ -177,9 +177,15 @@ def main():
                 joueurs[i], joueurs[i+1]=joueurs[i+1], joueurs[i]
                 
     print("\nClassement après le 1er match : \n")        
-    for i in joueurs:
-        print(i.name, i.f_name, i.score)    
-        #Appeler ensuite fonction/méthode de classement au terme des résultats du tour
+    """for i in range (cols):
+        col=[]
+        for j in range(rows):
+            if i==j:
+                col.append(1)
+            else:
+                col.append(0)
+        matchs_joues.append(col)"""
+    print(matchs_joues) 
             
 if __name__ == "__main__":
     main()
