042ec93a3278862102e669fdc45fa6a9573b641a
Matt 1968 <pascal.amann.perso@gmail.com>
2023-01-21 05:02:34 +0100

    Correction de bogues

--- vues/menu.py
+++ vues/menu.py
@@ -1,7 +1,7 @@
 class AffichageMenu:
     def __init__(self):
         self.menu = ['1. Nouveau tournoi et saisie des joueurs', '2. Affichage des matches','3. Saisie des resultats du tour',
-        '4. Match du tour suivant', '5. Afficher classement', '6. Affichage des joueurs', '7. Quitter']
+        '4. Match du tour suivant', '5. Afficher classement', '6. Affichage des joueurs', '0. Quitter']
         
     def display_menu(self):
         for i in self.menu:
