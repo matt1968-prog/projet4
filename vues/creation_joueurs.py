4f10511d32e16420aa59d4d70e74be2fb96c8ec0
Matt 1968 <pascal.amann.perso@gmail.com>
2023-01-24 21:23:30 +0100

    Légères modifications, tri ne fonctionne pas et joueurs n'est pas reconnu

--- vues/creation_joueurs.py
+++ vues/creation_joueurs.py
@@ -4,20 +4,24 @@ from modeles.player import Joueur
 class CreationJoueur:
 
     def __init__(self):
-        pass
+        
+        
 
     def creer_joueurs(self):
         joueurs = []
         dic_joueurs = {}
-         
+
         for i in range (1,9):
             score=0
             print(f"Saisie du joueur {i}\n")
             nom = input (f"Nom  du joueur {i} :\n")
             prenom = input(f"Prénom du joueur {i} : \n")
-            dob=input ("Date de naissance :") #vérif dans la VUE
-            sex=input("Sexe du joueur (H/F), par défaut H :")
-            if sexe="":
+            try:
+                dob=int(input ("Année de naissance :"))
+            except ValueError:
+                print("Vous devez saisir une année valide")
+            sexe=input("Sexe du joueur (H/F), par défaut H :")
+            if sexe=="" or sexe!="F":
                 sexe="H"
         
             while True:
@@ -27,28 +31,32 @@ class CreationJoueur:
                         break
                 except ValueError:
                     print("Le classement doit être un entier, positif et au moins égal à 1600")
-        
+            
             joueur = Joueur(nom, prenom, dob, sexe, rating, score)
-            joueur.to_dict(self)
             joueurs.append(joueur)
-            print(joueur.nom, joueur.prenom, joueur.dob, joueur.sexe, joueur.rating, joueur.score)
+            #joueur_dic = joueur.to_dict(joueur)
+            #dic_joueurs.append(joueur_dic)
+        #with open ('data.json', 'w') as fichier_joueurs:
+        #    json.dump(dic_joueurs, fichier_joueurs)
+            #print(joueur)
+            
+            #print(joueur.nom, joueur.prenom, joueur.dob, joueur.sexe, joueur.rating, joueur.score)
             #joueur = {"Nom": "nom", "Prenom": "prenom", "Date de naissance": "dob", "Sexe": "sex", "Classement ELO": "rating", "Classement tournoi": "score"}
             #joueurs.append(joueur)
             
-        #json.dump(joueurs, fjoueurs)
-          #  print(fj)    
+        #print(dic_joueurs)    
 
 
 
     #classement alphabétique   
-    def tri_alpha(self, joueurs):    
+    def tri_alpha(self):    
         joueurs.sort(key=lambda j: j.name, reverse=False)
         print("\nClassement par ordre alphabétique : \n")        
         for i in joueurs:
-            print(i.name, i.f_name, i.rating)
+            print(i.name, i_prenom, i.rating)
     
-  #classement par ELO   
-    def tri_elo(self, joueurs):
+    #classement par ELO   
+    def tri_elo(self):
         joueurs.sort(key=lambda j: j.rating, reverse=True)
         print("\nClassement par ELO : \n")        
         for i in joueurs:
