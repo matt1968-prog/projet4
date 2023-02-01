6ebdef365e46a504538c67c038d764a8319fe56f
Matt 1968 <pascal.amann.perso@gmail.com>
2023-02-01 21:10:08 +0100

    Ajout de la création d'un fichier JSON pour un nouveau tournoi

--- contrôleurs/creation_tournoi_31 janv.py
+++ contrôleurs/creation_tournoi_31 janv.py
@@ -8,12 +8,11 @@ class TournoiControleur:
     def create(self):
         vue = CreationTournoiView()
         data_tournoi = []
+        #ligne qui suit est un ajout
         nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours=vue.creer_tournoi()
-        #tournoi = Tournoi(nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours)
-        tournoi = Tournoi(nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, description, type_tournoi, nombre_tours)
+        tournoi = Tournoi(nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, type_tournoi, description, nombre_tours)
         data_tournoi.append(tournoi)
         
         with open ('data_tournoi.json', 'w') as file:
             data_tournoi = json.dumps([j.to_dict() for j in data_tournoi], indent = 4)
             file.write(data_tournoi)
-            print(data_tournoi)
