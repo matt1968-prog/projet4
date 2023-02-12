ca6ae4721785f710148c36d1e0e10db49122d550
Matt 1968 <pascal.amann.perso@gmail.com>
2023-02-12 17:16:55 +0100

    Version avec JSON pour l'enregistrement des joueurs

--- modeles/player.py
+++ modeles/player.py
@@ -1,11 +1,35 @@
+import json
+
+
 class Joueur:
-    def __init__(self, nom, prenom, dob, sexe, rating, score=0):
+    def __init__(self, nom, prenom, dob, sexe, id, rating, score=0):
         self.nom = nom
         self.prenom = prenom
         self.dob = dob
+        self.id = id
         self.rating = rating
         self.sexe = sexe
         self.score = score
-        
+
     def to_dict(self):
-        return {'nom':self.nom, 'prenom': self.prenom, 'Date de naissance': self.dob, 'ELO': self.rating, 'Sexe': self.sexe, 'Score': self.score}
+        return {'nom': self.nom, 'prenom': self.prenom, 'dob': self.dob, 'rating': self.rating, 'sexe': self.sexe, 'id': self.id, 'score': self.score}
+
+
+class JoueurDAO:
+    """def to_dict(self):
+        return {'nom': self.nom, 'prenom': self.prenom, 'dob': self.dob, 'rating': self.rating, 'sexe': self.sexe, 'id :': self.id, 'score': self.score}"""
+
+    def save(self, joueurs):
+        with open('data_joueurs.json', 'w') as file:
+            data_joueurs = json.dumps([j.to_dict() for j in joueurs], indent=4)
+            file.write(data_joueurs)
+
+    def load(self):
+        with open('data_joueurs.json', 'r') as file:
+            data_joueurs = json.loads(file.read())
+            joueurs = []
+            for d in data_joueurs:
+                j = Joueur(**d)
+                joueurs.append(j)
+            print(joueurs)
+            return joueurs
--- modeles/tournoi.py
+++ modeles/tournoi.py
@@ -1,14 +1,43 @@
+import json
+
+
 class Tournoi:
 
-    def __init__(self, nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, description, type_tournoi, nombre_tours =4 ):
+    def __init__(self, nom_tournoi, lieu_tournoi, date_debut_tournoi, date_fin_tournoi, description, type_tournoi, nombre_tours=4):
         self.nom_tournoi = nom_tournoi
         self.lieu_tournoi = lieu_tournoi
-        self.date_debut_tournoi = date_debut_tournoi    
+        self.date_debut_tournoi = date_debut_tournoi
         self.date_fin_tournoi = date_fin_tournoi
         self.nombre_tours = nombre_tours
         self.type_tournoi = type_tournoi
         self.description = description
-        self.nombre_tour=nombre_tours
+        self.nombre_tour = nombre_tours
 
     def to_dict(self):
         return {'Nom du tournoi': self.nom_tournoi, 'Lieu du Tournoi': self.lieu_tournoi, 'Date de début': self.date_debut_tournoi, 'Date de fin': self.date_fin_tournoi, 'Description': self.description, 'Type de tournoi': self.type_tournoi, 'Nombre de tours': '4'}
+
+
+class TournoiDAO:
+    def save(self, tournoi):
+        with open('data_tournoi.json', 'w') as file:
+            data = json.dumps([j.to_dict() for j in tournoi], indent=4)
+            file.write(data)
+
+    def load(self):
+        with open('data_tournoi.json', 'r') as file:
+            data = json.loads(file.read())
+            nouveau_tournoi = []
+            for d in data:
+                j = Tournoi(**d)
+                nouveau_tournoi.append(j)
+            print(nouveau_tournoi)
+            return nouveau_tournoi
+
+    """with open ('data_tournoi.json', 'w') as file:
+        data_tournoi = json.dumps([j.to_dict() for j in tournoi], indent = 4)
+        file.write(data_tournoi)
+        #print(data_tournoi)
+
+    with open('data_tournoi.json', 'r') as fichier_tournoi:
+        data_lues = json.load(fichier_tournoi)
+        print(data_lues)"""
