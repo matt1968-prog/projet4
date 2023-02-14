e88cb6480fbaba343d815933a25596089b0b8945
Matt 1968 <pascal.amann.perso@gmail.com>
2023-02-14 21:07:06 +0100

    Intégration des choix du menu et avec 4 joueurs pour création de matchs

--- controleurs/creation_joueurs.py
+++ controleurs/creation_joueurs.py
@@ -8,11 +8,11 @@ class JoueurControleur:
     def create(self):
         vue = CreationJoueurView()
         joueurs = []
-        for i in range(1, 4):
+        for i in range(1, 5):
             nom, prenom, dob, sexe, id, rating = vue.creer_joueur()
             joueur = Joueur(nom, prenom, dob, sexe, id, rating, score=0)
             joueurs.append(joueur)
-            print(joueurs)
+            #print(joueurs)
 
         dao = JoueurDAO()
         dao.save(joueurs)
