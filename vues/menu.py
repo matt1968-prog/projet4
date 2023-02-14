1e42508b27416129ef3ef83c1c857dae7079026a
Matt 1968 <pascal.amann.perso@gmail.com>
2023-02-14 21:06:20 +0100

    Intégration des choix du menu et avec 4 joueurs pour création de matchs

--- vues/menu.py
+++ vues/menu.py
@@ -1,7 +1,7 @@
 class AffichageMenu:
     def __init__(self):
-        self.menu = ['1. Nouveau tournoi et saisie des joueurs', '2. Affichage des matches', '3. Saisie des resultats du tour',
-                     '4. Matchs du tour suivant', '5. Affichage du classement', '6. Affichage des joueurs', '7. Affichage du tournoi', '0. Quitter']
+        self.menu = ['1. Nouveau tournoi et saisie des joueurs', '2. Matchs du 1er tour', '3. Saisie des resultats du 1er tour',
+                     '4. Matchs du tour suivant', '5. Affichage du classement', '6. Affichage des joueurs', '7. Affichage des données du tournoi', '0. Quitter']
 
     def display_menu(self):
         for i in self.menu:
