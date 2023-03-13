0f031e2d849db1fd2d51b340f4a1c178c5158a94
Matt 1968 <pascal.amann.perso@gmail.com>
2023-03-13 18:43:33 +0100

    Moficication des noms de retour

--- modeles/tournoi.py
+++ modeles/tournoi.py
@@ -8,13 +8,12 @@ class Tournoi:
         self.lieu_tournoi = lieu_tournoi
         self.date_debut_tournoi = date_debut_tournoi
         self.date_fin_tournoi = date_fin_tournoi
-        self.nombre_tours = nombre_tours
         self.type_tournoi = type_tournoi
         self.description = description
-        self.nombre_tour = nombre_tours
+        self.nombre_tours = nombre_tours
 
     def to_dict(self):
-        return {'Nom du tournoi': self.nom_tournoi, 'Lieu du Tournoi': self.lieu_tournoi, 'Date de début': self.date_debut_tournoi, 'Date de fin': self.date_fin_tournoi, 'Description': self.description, 'Type de tournoi': self.type_tournoi, 'Nombre de tours': '4'}
+        return {'nom_tournoi': self.nom_tournoi, 'lieu_tournoi': self.lieu_tournoi, 'date_debut_tournoi': self.date_debut_tournoi, 'date_fin_tournoi': self.date_fin_tournoi, 'description': self.description, 'type_tournoi': self.type_tournoi, 'nombre_tours': '4'}
 
 
 class TournoiDAO:
