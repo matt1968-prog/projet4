b9f76f70ff03b401ee4b38a319b48f7416b01e3b
Matt 1968 <pascal.amann.perso@gmail.com>
2023-03-24 20:28:27 +0100

    Insertion des dates, non fonctionnel. Tableau matchs disputés partiellement fonctionnel.

--- tournoi_orig_20mars.py
+++ tournoi_orig_20mars.py
@@ -13,6 +13,8 @@ from controleurs.creation_tournoi import TournoiControleur
 from pprint import pprint
 from typing import List
 from uuid import UUID, uuid4
+from modeles.tour import Round
+from controleurs.tour import ControleurTour
 
 
 def main():
@@ -25,7 +27,8 @@ def main():
     round_number = 1
     nouveau_tournoi = False
     matchs = []
-    nom_base_tour="Round "
+    nom_base_tour = "Round "
+    
     while choix != 0:
         menu_ecran = AffichageMenu()
         menu_ecran.display_menu()
@@ -51,10 +54,6 @@ def main():
                     print("Création du tournoi\n")
                     controleur_tournoi = TournoiControleur()
                     controleur_tournoi.create()
-                    #tournoi_cree=True
-                    # affichage_tournoi=afficher_tournoi() ne trouve pas afficher_tournoi
-                    #nouveau_tournoi = Tournoi()
-                    #nouveau_tournoi.afficher_tournoi()
                     print()
 
                 elif choix == 2:
@@ -88,33 +87,44 @@ def main():
                         joueurs = dao.load()
                         joueurs.sort(key=lambda j: j.rating, reverse=True)
 
+                    # matchs du premier tour (système suisse)
                     if round_number == 1:
+                        # création du tour ? MVC
+                        #round_name = nom_base_tour +str(round_number)
+                        #round = Round(id, round_name, round_number, []) #INSERER PARAMETRES
+                        #print(f"Nom du tour : {round_name}\n")
+                        #controleur_tour = ControleurTour()
+                        #controleur_tour.create()
                         print(f"Matchs du tour {round_number}\n")
                         for i in range(0, 2):
                             print(f"Match  {i+1} : ")
                             print(joueurs[i].nom + " vs " + joueurs[i+2].nom)
                             match = ([joueurs[i].nom, joueurs[i].score], [joueurs[i+2].nom, joueurs[i+2], joueurs[i+2].score])
-                            matchs.append(match)
+                            round.matchs.append(match)
                         print()
                     
                     # à partir du tour 2, appariement en fonction des scores dans le tournoi (1 contre 2, 3 vs 4 etc.)
                     elif round_number > 1 and round_number < MAX_ROUND:
+                        # création du tour  CONTROLEUR OU CLASSE SIMPLE ?
+                        round_name = nom_base_tour +str(round_number)
+                        print(f"Nom du tour : {round_name}\n")
+                        controleur_tour = TourControleur()
+                        controleur_tour.create()
                         print(f"Matchs du tour {round_number}\n")
-                        #round_number += 1
+                        tour = Round(round_name, round_number, matchs)
                         joueurs.sort(key=lambda j: j.score)
                         j=0
                         for i in range(0, 2):
                             print(f"Match  {i+1} : ")
                             print(joueurs[j].nom + " vs " + joueurs[j+1].nom)
-                            match = ([joueurs[j].nom], [joueurs[j+1].nom])
+                            match = ([joueurs[j].nom], joueurs[j].score, [joueurs[j+1].nom, joueurs[j+1].score])
                             matchs.append(match)
                             j += 2
                         print()
 
                         match = ([joueurs[i].nom, joueurs[i].score], [joueurs[i+1].nom, joueurs[i+1], joueurs[i+1].score])
                         matchs.append(match)
-                        round_name = nom_base_tour +str(round_number)
-                        print(f"Nom du tour : {round_name}")
+                        
 
                 elif choix == 4:
                     print("Pour les résultats, 1=gain premier joueur, 2=gain second joueur, 0=match nul\n")
@@ -124,7 +134,7 @@ def main():
                         for i in range(0, 2):
                             print(joueurs[i].nom + " vs " + joueurs[i+2].nom)
                             resultat = int(input("Résultat : "))
-                            matchs_joues[i][i+2]=1
+                            
                             if resultat == 1:
                                 joueurs[i].score += 1
                             elif resultat == 2:
@@ -135,7 +145,9 @@ def main():
                             else:
                                 print("Donnée non valide")
                                 print(str(joueurs[i].score) + " " + str(joueurs[i+2].score))# -> vue pour l'affichage, controler pour modif des scores
-                            #matchs_joues[i][i+2]=1    
+                        
+                        matchs_joues[0][2]=1
+                        matchs_joues[1][3]=1
                     
                     elif round_number > 1 and round_number < 4: 
                         j=0
@@ -153,85 +165,34 @@ def main():
                                 joueurs[j+1].score += 0.5
                             else:
                                 print("Donnée non valide")
-                            matchs_joues[j][j+1] = 1
-                            matchs_joues[j+1][j] = 1
                             j += 2
-                            print(f" j = {j}")
-                            #matchs_joues[j][j+1] = 1
-                            #matchs_joues[j+1][j] = 1
-                    else:
-                        print("Tous les tours ont été joués. Le tournoi est terminé.")        
-                    print (matchs_joues)
-                    print()
-                    # round_number += 1
-                    
-                    # maj des matchs disputés A MODIFIER ET REMPLACER PAR TABLEAU A DEUX DIMENSIONS
-                    """rows, cols = (NBRE_JOUEURS, NBRE_JOUEURS)
-                    for i in range(cols):
-                        #col = []
-                        for j in range(rows):
-                                col.append(1)
-                        matchs_joues.append(col)
-                    print(matchs_joues)  #  à fin d'information seulement"""
-                    
-                    #méthode avec tableau 2D
-                    """for ligne in range(4):
-                        nvligne = []
-                        for col in range(4):
-                            if ligne == col:
-                                nvligne.append(1)
-                            else:
-                                nvligne.append(0)
-                        matchs_joues.append(nvligne)
-                    
-                    for ligne in matchs_joues:
-                        print(ligne)"""
 
+                            matchs_joues[0][1] = 1
+                            matchs_joues[2][3] = 1
+                            matchs_joues[3][2] = 1
+
+                    
+                        print (matchs_joues)
+                        print()
+
+                    elif round_number >3:
+                        print("Tous les tours ont été joués. Le tournoi est terminé.")
+                        continue
 
                     #enregistrement après saisie des résultats du tour achevé
                     with open('data_joueurs.json', 'w') as file:
                         data_joueurs = json.dumps([j.to_dict() for j in joueurs], indent=4)
                         file.write(data_joueurs)
 
-                    # actualisation du tableau des matchs joués
-                    """rows, cols = (NBRE_JOUEURS, NBRE_JOUEURS)
-                    for i in range(cols):
-                        #col = []
-                        for j in range(rows):
-                            col.append(1)
-                        matchs_joues.append(col)
-                        #print("Matchs joués {matchs_joues}")"""
-                    
-                    # méthode avec tableau à deux dimensionsgrille = []
-
-                    """for ligne in range(4):
-                        nvligne = []
-                        for col in range(4):
-                            if ligne == col:
-                                nvligne.append(1)
-                            else:
-                                nvligne.append(0)
-                            grille.append(nvligne)
-                    
-                    for ligne in grille:
-                        print(ligne)"""
-
-                    # méthode avec tableau 2D
-
                     # màj classement après le tour
                     joueurs.sort(key=lambda j: j.score, reverse=True)
 
                     print(f"Classement après le tour {round_number}\n")
                     for i in joueurs:
-                        print(i.nom, i.prenom, i.score)
+                        print(f"{i.nom} {i.prenom} Score : {i.score}")
                     print()
 
                     round_number += 1
-
-                    """# enregistrement du fichier AJOUT DU 17 MARS AU SOIR A VERIFIER
-                    with open('data_joueurs.json', 'w') as file:
-                        data_joueurs = json.dump(file)
-                        file.write(data_joueurs)"""
 
                 elif choix == 5:
                     if round_number < MAX_ROUND:
@@ -249,7 +210,6 @@ def main():
                     dao = JoueurDAO()
                     joueurs = dao.load()
                     joueurs.sort(key=lambda j: j.rating, reverse=True)
-                    #print(joueurs)
                     print("\nListe par classement ELO : \n")
                     for i in joueurs:
                         print(f"Nom : {i.nom} Prénom {i.prenom} ELO : {i.rating}")
